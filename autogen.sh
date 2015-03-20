d53d4115d8a690814217dfe3b5de05d519b5213e
Thierry Reding <treding@nvidia.com>
2015-03-20 13:42:11 +0100

    buildsys: Allow autogen.sh to be run out-of-tree
    Being able to run autogen.sh out-of-tree is useful to bootstrap an
    out-of-tree build.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -e
 
-autoreconf --install --symlink
-./configure $@
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
+ORIGDIR=`pwd`
+cd "$srcdir"
+
+autoreconf --install --symlink || exit 1
+cd "$ORIGDIR" || exit $?
+
+$srcdir/configure "$@"
